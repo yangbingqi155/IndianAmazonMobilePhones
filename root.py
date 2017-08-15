b1e588f87d929565b890523df6f842cf71fc3374
yangbinqi <ybq@outlook.com>
2017-08-15 09:31:04 +0800

    fix bugs

--- root.py
+++ root.py
@@ -27,7 +27,10 @@ import db_product
 
 sys.setrecursionlimit(1000000) 
 has_p_info_pages=0
-p_list_url='https://www.amazon.in/s/ref=s9_acss_bw_cts_VodooFS_T3L4_w?rh=n%3A976419031%2Cn%3A!976420031%2Cn%3A1389401031%2Cn%3A1389432031%2Cn%3A1805560031%2Cp_36%3A1318505031&qid=1484135884&bbn=1805560031&low-price=10%2C000&high-price=15%2C000&x=16&y=9&pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-7&pf_rd_r=V9060E9AEDXMKC8SMK73&pf_rd_t=101&pf_rd_p=476f8f1a-15ac-4693-b157-8657b9ebf7e1&pf_rd_i=1389401031'
+p_list_url='https://www.amazon.in/s/ref=s9_acss_bw_cts_VodooFS_T1L4_w?rh=n%3A976419031%2Cn%3A!976420031%2Cn%3A1389401031%2Cn%3A1389432031%2Cp_98%3A10440597031&qid=1484135102&bbn=1389432031&low-price=&high-price=5%2C000&x=6&y=10&pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-7&pf_rd_r=N8FAX8586W73XVCYYS7Y&pf_rd_t=101&pf_rd_p=476f8f1a-15ac-4693-b157-8657b9ebf7e1&pf_rd_i=1389401031'
+p_list_url1='https://www.amazon.in/s/ref=s9_acss_bw_cts_VodooFS_T2L4_w?rh=n%3A976419031%2Cn%3A!976420031%2Cn%3A1389401031%2Cn%3A1389432031%2Cn%3A1805560031&qid=1484135856&bbn=1805560031&low-price=5%2C000&high-price=10%2C000&x=3&y=12&pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-7&pf_rd_r=N8FAX8586W73XVCYYS7Y&pf_rd_t=101&pf_rd_p=476f8f1a-15ac-4693-b157-8657b9ebf7e1&pf_rd_i=1389401031'
+p_list_url2='https://www.amazon.in/s/ref=s9_acss_bw_cts_VodooFS_T3L4_w?rh=n%3A976419031%2Cn%3A!976420031%2Cn%3A1389401031%2Cn%3A1389432031%2Cn%3A1805560031%2Cp_36%3A1318505031&qid=1484135884&bbn=1805560031&low-price=10%2C000&high-price=15%2C000&x=16&y=9&pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-7&pf_rd_r=N8FAX8586W73XVCYYS7Y&pf_rd_t=101&pf_rd_p=476f8f1a-15ac-4693-b157-8657b9ebf7e1&pf_rd_i=1389401031'
+p_list_url3='https://www.amazon.in/s/ref=s9_acss_bw_cts_VodooFS_T4L4_w?rh=i%3Aelectronics%2Cn%3A976419031%2Cn%3A%21976420031%2Cn%3A1389401031%2Cn%3A1389432031%2Cn%3A1805560031%2Cp_98%3A10440597031%2Cp_36%3A1500000-99999999&bbn=1805560031&rw_html_to_wsrp=1&pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-7&pf_rd_r=N8FAX8586W73XVCYYS7Y&pf_rd_t=101&pf_rd_p=476f8f1a-15ac-4693-b157-8657b9ebf7e1&pf_rd_i=1389401031'
 #在产品列表面获取产品详细页面的url,并且返回下一页的URL
 def get_product_info_urls(p_list_page_url):
 	next_page_url= ''
@@ -48,9 +51,11 @@ def get_product_info_urls(p_list_page_url):
 			#如果最后一页
 			if len(content.xpath("//span[@class='pagnRA1']"))>0:
 				print 'The end list page :'+p_list_page_url
-				return [[],'',True]
+				logging.info('The end list page :'+p_list_page_url)
+				return [map(lambda x:x.get("href"),result) if result!=None and len(result)>0 else None,'',True]
 			else:
 				#may be server think this request client is a robot
+				logging.info('Can\'t find next list page url,but can open the current list page url.')
 				print 'Can\'t find next list page url,but can open the current list page url.'
 				return [None,'',False]
 	except  requests.exceptions.Timeout as e:
@@ -152,6 +157,7 @@ def go_p_list_page(p_list_url):
 		change_proxy()
 		if p_info_urls==None:
 			continue
+		
 		all_thread=[]
 		
 		for info_url in p_info_urls:
@@ -162,10 +168,12 @@ def go_p_list_page(p_list_url):
 		for thread in all_thread:
 			thread.join()
 		if 	next_page_url=='':
-			print 'end of product list page,break:'+url+",p_info_urls length:"+str(p_info_urls)
+			logging.info('end of product list page,break:'+url)
+			print 'end of product list page,break:'+url
 			break
 		else:
 			url=next_page_url
+			logging.info('product list next page:'+url+",p_info_urls length:")
 			print 'product list next page:'+url+",p_info_urls length:"
 #添加产品到数据库
 def add_product(info_url):
@@ -178,15 +186,18 @@ def add_product(info_url):
 				p_data= json.dumps(model,default=model_product.productmodel2dict)
 				db_product.add(model)
 			except BaseException as e:
-				print 'error product info url:'+info_url
-				print 'error product info data:'+p_data
 				logging.exception('error product info url:'+info_url)
 				logging.exception('error product info data:'+p_data)
+				print 'error product info url:'+info_url
+				print 'error product info data:'+p_data
 				raise e
+			logging.info(p_data+"\n")
 			print(p_data+"\n")
 			break
 		else:
+		logging
 			print 'can\'t get product info,product info url:'+info_url
+			logging.info('can\'t get product info,product info url:'+info_url)
 			continue
 	
 #启用代理
@@ -200,6 +211,7 @@ def set_proxy():
 	verified_proxies_num=len(newest_verified_proxy_ips)
 	if verified_proxies_num==0:
 		config.enable_proxy=False
+		logging.info('Can\' enable proxy,because no verified proxies\n')
 		print 'Can\' enable proxy,because no verified proxies\n'
 		return
 	else:
@@ -209,12 +221,14 @@ def set_proxy():
 	verified_proxy=newest_verified_proxy_ips[0];
 	config.proxies={verified_proxy.Protocol:verified_proxy.IP+":"+str(verified_proxy.Port)}
 	config.enable_proxy=True
+	logging.info('Success enable proxy:'+str(config.current_proxy_index)+'\n')
 	print 'Success enable proxy:'+str(config.current_proxy_index)+'\n'
 #切换代理信息
 def change_proxy():
 	# if config.enable_proxy==False:
 		# print 'didn\'t enable ip proxy,can\'t change ip proxy.'
 		# return
+	logging.info('change ip proxy....\n')
 	print 'change ip proxy....\n'
 	verified_proxies_num=libhttpproxy.get_verified_proxies_num()
 	if config.current_proxy_index+1>=verified_proxies_num:
@@ -223,10 +237,20 @@ def change_proxy():
 		config.current_proxy_index=config.current_proxy_index+1
 	set_proxy()
 	if config.enable_proxy==True:
+		logging.info('Success change ip proxy.\n' )
 		print 'Success change ip proxy.\n' 
-		
+def set_logging()：
+	logging.basicConfig(level=logging.DEBUG,
+                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                datefmt='%a, %d %b %Y %H:%M:%S',
+                filename='amazon_mobile.log',
+                filemode='w')
+set_logging()
 set_proxy_enable()
 go_p_list_page(p_list_url)
+go_p_list_page(p_list_url1)
+go_p_list_page(p_list_url2)
+go_p_list_page(p_list_url3)
 #get_product_info('http://www.amazon.in/dp/B074PWHB1R/ref=s%20r_1_1014/262-4008005-9308516?s=electronics&ie=UTF8&qid=1502377276&sr=1-1014')
 #print json.dumps(get_product_info('http://www.amazon.in/dp/B074PWHB1R/ref=s%20r_1_1014/262-4008005-9308516?s=electronics&ie=UTF8&qid=1502377276&sr=1-1014'),default=model_product.productmodel2dict)
 #html=basic.get_html('http://www.amazon.in/Forme-N9-Selfie-Wireless-Mobile/dp/B071G2DSSC/ref=sr_1_142/257-5514151-9158917?s=electronics&rps=1&ie=UTF8&qid=1501400614&sr=1-142')
